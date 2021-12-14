7d80e7493dd7f68e78e4a646b68e9d721da5eba0
nadzhimmmm <92924872+nadzhimmmm@users.noreply.github.com>
2021-12-14 22:37:39 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("1)Ввести количество элементов.");
+        System.out.println("0)Выход.");
+        Scanner in = new Scanner(System.in);
+        int quit;
+        while (!in.hasNextInt()) {
+            System.out.println("Ошибка. Неверный ввод. Введите число.");
+            in.next();
+        }
+        quit = in.nextInt();
+        while (quit < 0 || quit > 1) {
+            System.out.println("Ошибка. Введите число 0 или 1.");
+            quit = in.nextInt();
+        }
+        while (quit != 0) {
+            List<Integer> arrayList = new ArrayList<>();
+            List<Integer> linkedList = new LinkedList<>();
+            int N;
+            while (!in.hasNextInt())
+            {
+                System.out.println("Ошибка. Неверный ввод. Введите число.");
+                in.next();
+            }
+            N = in.nextInt();
+            System.out.println("|Method         |ArrayList           |LinkedList          | \n" +
+                    "|---------------|--------------------|--------------------|");
+
+            System.out.print("|add            |");
+            System.out.println(printResult(TestCollection.testAdd(arrayList, N), TestCollection.testAdd(linkedList, N)));
+
+            System.out.print("|remove(first)  |");
+            System.out.println(printResult(TestCollection.testRemoveFirst(arrayList, N), TestCollection.testRemoveFirst(linkedList, N)));
+
+            System.out.print("|remove(last)   |");
+            System.out.println(printResult(TestCollection.testRemoveLast(arrayList, N), TestCollection.testRemoveLast(linkedList, N)));
+
+            System.out.print("|remove(center) |");
+            System.out.println(printResult(TestCollection.testRemoveCenter(arrayList, N), TestCollection.testRemoveCenter(linkedList, N)));
+
+            System.out.print("|get            |");
+            System.out.println(printResult(TestCollection.testGet(arrayList, N), TestCollection.testGet(linkedList, N)));
+
+            System.out.print("|get(first)     |");
+            System.out.println(printResult(TestCollection.testGetFirst(arrayList, N), TestCollection.testGetFirst(linkedList, N)));
+
+            System.out.print("|get(last)      |");
+            System.out.println(printResult(TestCollection.testGetLast(arrayList, N), TestCollection.testGetLast(linkedList, N)));
+
+            System.out.print("|get(center)    |");
+            System.out.println(printResult(TestCollection.testGetCenter(arrayList, N), TestCollection.testGetCenter(linkedList, N)));
+
+
+            System.out.print("|clear          |");
+            System.out.println(printResult(TestCollection.testClear(arrayList, N), TestCollection.testClear(linkedList, N)));
+        }
+    }
+
+    private static String printResult(long res1, long res2) {
+        return String.format("%20d|%20d|\n|---------------|--------------------|--------------------|", res1, res2);
+    }
+}
---
+++ src/TestCollection.java
@@ -0,0 +1,160 @@
+import java.util.Collection;
+import java.util.List;
+/**
+ * @author Mokhammad Nadzhim
+ * @version 1.0
+ */
+
+/**
+ * A class that calculates the running time of operations in arraylist and linkedlist
+ */
+public class TestCollection {
+
+    private TestCollection(){}
+
+    /**
+     * measures the execution time of n addition operations
+     *
+     * @param testList collection for the test
+     * @param n        number of necessary elements
+     * @return execution time
+     */
+    public static long testAdd(List<Integer> testList, int n) {
+        long start = System.nanoTime();
+        for (int i = 0; i < n; i++)
+            testList.add(i);
+        return System.nanoTime() - start;
+    }
+
+    /**
+     * measures the time of n deletion operations from the first position
+     *
+     * @param testList collection for the test
+     * @param n        number of necessary elements
+     * @return execution time
+     */
+    public static long testRemoveFirst(List<Integer> testList, int n) {
+        addTo(n, testList);
+        long start = System.nanoTime();
+        for (int i = 0; i < n; i++)
+            testList.remove(0);
+        return System.nanoTime() - start;
+    }
+
+    /**
+     * measures the time of n deletion operations from the last position
+     *
+     * @param testList collection for the test
+     * @param n        number of necessary elements
+     * @return execution time
+     */
+    public static long testRemoveLast(List<Integer> testList, int n) {
+        addTo(n, testList);
+        long start = System.nanoTime();
+        for (int i = n - 1; i >= 0; i--)
+            testList.remove(i);
+        return System.nanoTime() - start;
+    }
+
+    /**
+     * measures the time of n removal operations from the middle
+     *
+     * @param testList collection for the test
+     * @param n        number of necessary elements
+     * @return execution time
+     */
+    public static long testRemoveCenter(List<Integer> testList, int n) {
+        addTo(n, testList);
+        long start = System.nanoTime();
+        int halfN = n / 2;
+        for (int i = 0; i < n; i++)
+            testList.remove(halfN - i / 2 - 1);
+        return System.nanoTime() - start;
+    }
+
+    /**
+     * we measure the time of receipt from all n positions
+     *
+     * @param testList collection for the test
+     * @param n        number of necessary elements
+     * @return execution time
+     */
+    public static long testGet(List<Integer> testList, int n) {
+        addTo(n, testList);
+        long start = System.nanoTime();
+        for (int i = 0; i < n; i++)
+            testList.get(i);
+        return System.nanoTime() - start;
+    }
+
+    /**
+     * measures the time of n receiving operations from the first position
+     *
+     * @param testList collection for the test
+     * @param n        number of necessary elements
+     * @return execution time
+     */
+    public static long testGetFirst(List<Integer> testList, int n) {
+        addTo(n, testList);
+        long start = System.nanoTime();
+        for (int i = 0; i < n; i++)
+            testList.get(0);
+        return System.nanoTime() - start;
+    }
+
+    /**
+     * measures the time of n receiving operations from the last position
+     *
+     * @param testList collection for the test
+     * @param n        number of necessary elements
+     * @return execution time
+     */
+    public static long testGetLast(List<Integer> testList, int n) {
+        addTo(n, testList);
+        long start = System.nanoTime();
+        for (int i = n - 1; i >= 0; i--)
+            testList.get(n-1);
+        return System.nanoTime() - start;
+    }
+
+    /**
+     * measures the time of n operations of getting from the middle
+     *
+     * @param testList collection for the test
+     * @param n        number of necessary elements
+     * @return execution time
+     */
+    public static long testGetCenter(List<Integer> testList, int n) {
+        addTo(n, testList);
+        long start = System.nanoTime();
+        int halfN = n / 2;
+        for (int i = 0; i < n; i++)
+            testList.get(halfN);
+        return System.nanoTime() - start;
+    }
+
+    /**
+     * we measure the cleaning time
+     *
+     * @param testList collection for the test
+     * @param n        number of necessary elements
+     * @return execution time
+     */
+    public static long testClear(List<Integer> testList, int n) {
+        addTo(n, testList);
+        long start = System.nanoTime();
+        testList.clear();
+        return System.nanoTime() - start;
+    }
+
+    /**
+     * complements the elements TO the required amount
+     *
+     * @param count  required quantity
+     * @param addCol collection to add
+     */
+    private static void addTo(int count, Collection<Integer> addCol) {
+        for (int i = addCol.size(); i < count; i++)
+            addCol.add(i);
+    }
+}
